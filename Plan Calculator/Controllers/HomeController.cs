4fd9f1e1e8c5e1f62f55ae1fb34780db12862604
hebeiwork <hebeiwork@gmail.com>
2019-10-21 18:49:36 +1100

    Add Calculator function

--- Plan Calculator/Controllers/HomeController.cs
+++ Plan Calculator/Controllers/HomeController.cs
@@ -10,13 +10,49 @@ namespace Plan_Calculator.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Index([Bind("price,purchaseDate")] Plan plan)
         {
+            ViewBag.plans = new List<Plan>();
+            if (plan.price >= 10000 || plan.price< 100)
+            {
+                ViewBag.Message = "Plans not offered"; 
+                
+            }else if (plan.price >= 100 && plan.price < 1000)
+            {
+                Plan planA = new Plan();
+                planA.price = plan.price;
+                planA.purchaseDate = plan.purchaseDate;
+                planA.initDepositeRate = 0.2;
+                planA.installmentInterval = 15;
+                planA.installments = 5;
+                ViewBag.plans.Add(planA);
+
+                Plan planB = new Plan();
+                planB.price = plan.price;
+                planB.purchaseDate = plan.purchaseDate;
+                planB.initDepositeRate = 0.3;
+                planB.installmentInterval = 15;
+                planB.installments = 4;
+                ViewBag.plans.Add(planB);
+            }
+            else if (plan.price >= 1000 && plan.price < 10000)
+            {
+                Plan planA = new Plan();
+                planA.price = plan.price;
+                planA.purchaseDate = plan.purchaseDate;
+                planA.initDepositeRate = 0.25;
+                planA.installmentInterval = 30;
+                planA.installments = 4;
+                ViewBag.plans.Add(planA);
+            }
+
             return View();
         }
 
---
+++ Plan Calculator/Models/Plan.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plan_Calculator.Models
+{
+    public class Plan
+    {
+        public double price { get; set; }
+        public DateTime purchaseDate { get; set; }
+
+        public double initDepositeRate { get; set; }
+        public double installmentInterval { get; set; }
+        public int installments { get; set; }
+
+        public Double initDeposite()
+        {
+            return price * initDepositeRate;
+        }
+
+        public Double installmentAmount()
+        {
+            return price * (1 - initDepositeRate) / installments;
+        }
+
+        public List<DateTime> installmentDates()
+        {
+            DateTime lastDate = purchaseDate.AddDays(installmentInterval);
+            double installmentGapDays = installmentInterval / installments;
+            List<DateTime> payDates = new List<DateTime>();
+
+            for (int i = 1; i < installments; i++)
+            {
+                DateTime payDate = purchaseDate.AddDays(installmentGapDays * i);
+                payDates.Add(payDate);
+            }
+
+            payDates.Add(lastDate);
+            return payDates;
+        }
+
+    }
+}
